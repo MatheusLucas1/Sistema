e8b9d892a6d72530de2160cd9cff0d846a88a1c6
Matheus <Matheus.lucas1105@gmail.com>
2023-08-24 17:55:54 -0300

    cadastro de pessoas candidatas e eleitoras

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -4,41 +4,34 @@ import java.util.ArrayList;
 
 public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
-  private final ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<PessoaCandidata>();
-
-  private final ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<PessoaEleitora>();
-
-  private final ArrayList<String> cpfsComputados = new ArrayList<String>();
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<>();
+  private ArrayList<String> cpfsComputados = new ArrayList<>();
 
   @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
-    for (PessoaCandidata pessoasCandidata : pessoasCandidatas) {
 
-      int numeroCandidato = pessoasCandidata.getNumero();
-
-      if (numeroCandidato == numero) {
-        System.out.println("Pessoa candidata já cadastrada!");
-      } else {
-        PessoaCandidata candidato = new PessoaCandidata(nome, numero);
-        pessoasCandidatas.add(candidato);
+    for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+      if (pessoaCandidata.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
       }
     }
+    PessoaCandidata novoCandidato = new PessoaCandidata(nome, numero);
+    pessoasCandidatas.add(novoCandidato);
   }
 
   @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
 //    int cpfToInt = Integer.parseInt(cpf);
     for (PessoaEleitora pessoasEleitora : pessoasEleitoras) {
-
-      String cpfEleitor = pessoasEleitora.getCpf();
-
-      if (cpfEleitor.equals(cpf)) {
+      if (pessoasEleitora.getCpf().equals(cpf)) {
         System.out.println("Pessoa eleitora já cadastrada!");
-      } else {
-        PessoaEleitora eleitor = new PessoaEleitora(nome, cpf);
-        pessoasEleitoras.add(eleitor);
+        return;
       }
     }
+    PessoaEleitora novaPessoaEleitora = new PessoaEleitora(nome, cpf);
+    pessoasEleitoras.add(novaPessoaEleitora);
   }
 
   @Override
--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,14 +1,15 @@
 package com.betrybe.sistemadevotacao;
 
+
 public abstract class Pessoa {
-    protected String nome;
 
-    public String getNome() {
-        return this.nome;
-    }
+  protected String nome;
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+  public String getNome() {
+    return nome;
+  }
 
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
